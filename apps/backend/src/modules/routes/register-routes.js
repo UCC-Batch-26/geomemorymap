40a6894967583f987949443ff656a0b72927a8ac
dencioo <paldeanharley@gmail.com>
2025-09-23 23:21:22 +0800

    feat: add register route and connect controller

---
+++ apps/backend/src/modules/routes/register-routes.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { register } from '#modules/controllers/auth-register-controller.js';
+
+const router = express.Router();
+
+router.post('/', register);
+
+export default router;
