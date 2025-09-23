5202222cdf65c0ca0f851697d5653c816e95fa3d
Peter <pedrojuana08@gmail.com>
2025-09-23 15:03:25 +0800

    login added

---
+++ apps/backend/src/modules/samples/controllers/loginController.js
@@ -0,0 +1,29 @@
+import { User } from '/uplift/geomemory-app/geomemorymap/apps/backend/src/modules/samples/models/userSchema.js';
+import jwt from 'jsonwebtoken';
+
+const JWT_SECRET = 'GeoMemoryMap';
+
+export async function login(request, response){
+  try {
+    const user = request.user;
+
+    // token 
+      const token = jwt.sign(
+        { id: user._id, username: user.username},
+        JWT_SECRET,
+        { expiresIn: '7D'} // expires in 7 days
+      );
+
+    return ressponse.status(200).json({
+      message: `Welcome ${user.username}`,
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+      },
+    });
+  } catch (err){
+    console.error('Login Error:', err);
+    response.status(500).json({ message: 'Internal server error'});
+  }
+}
--- apps/backend/src/modules/samples/routes.js
+++ apps/backend/src/modules/samples/routes.js
@@ -2,11 +2,15 @@ import { Router } from 'express';
 import { addSample } from './controllers/add-sample.js';
 import { getAllSamples } from './controllers/get-all-samples.js';
 import { getSample } from './controllers/get-sample.js';
+import { validateLogin } from '#modules/common/middleware/loginMiddleware.js';
+import { login } from './controllers/loginController.js';
+
 
 const router = new Router();
 
 router.post('/', addSample);
 router.get('/', getAllSamples);
 router.get('/:id', getSample);
+routes.post('/login', validateLogin, login);
 
 export default router;
