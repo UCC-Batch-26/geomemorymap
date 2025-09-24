61acc4f3d6a6521d39617ba54a698e55a141a4c5
dencioo <paldeanharley@gmail.com>
2025-09-24 15:46:02 +0800

    feat: add the register middleware in routes

--- apps/backend/src/modules/routes/register-routes.js
+++ apps/backend/src/modules/routes/register-routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { register } from '#modules/controllers/auth-register-controller.js';
+import { validateRegister } from '#modules/common/middleware/validate-register-middleware.js';
 
 const router = express.Router();
 
-router.post('/register', register);
+router.post('/register', validateRegister, register);
 
 export default router;
