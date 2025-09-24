d355b1480261e4f597bd531bd5d44b813c696917
Nathaniel Dacasin <nathandacasin01@gmail.com>
2025-09-24 20:50:56 +0800

    feat: changes folder

--- apps/frontend/src/app.jsx
+++ apps/frontend/src/app.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router';
-import { LoginPage } from '@/modules/sample/pages/login-page';
-import { RegistrationForm } from '@/modules/sample/pages/registration-form';
+import { LoginPage } from '@/modules/pages/login-page';
+import { RegistrationForm } from '@/modules/pages/registration-form';
 
 const router = createBrowserRouter([
     { path: '/', element: <LoginPage />},
