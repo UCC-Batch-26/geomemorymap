7363ad62769ecfe2e88e579671c775126ce1f5cf
dencioo <paldeanharley@gmail.com>
2025-09-24 15:44:45 +0800

    feat: add a validation middleware for registration

---
+++ apps/backend/src/modules/common/middleware/validate-register-middleware.js
@@ -0,0 +1,30 @@
+export function validateRegister(request, response, next) {
+  const { username, email, password } = request.body;
+
+  if (!username || !email || !password ) {
+    return response.status(400).json({
+      message: "Username, email, and password are required",
+    })
+  }
+
+  if (username.length < 3 || username.length > 20) {
+    return response.status(400).json({
+      message: "Username must be between 3 and 20 characters",
+    });
+  }
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    return response.status(400).json({
+      message: "Invalid email format",
+    })
+  }
+
+  if (password.length < 6) {
+    return response.status(400).json({
+      message: "Password must be atleast 6 characters long",
+    })
+  }
+
+  next();
+}
