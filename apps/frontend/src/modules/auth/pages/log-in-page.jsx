28322bb7fc31077ef9c6671d0f2adb75ef022e6f
Nathaniel Dacasin <nathandacasin01@gmail.com>
2025-09-25 20:19:33 +0800

    feat: signup and login

--- apps/frontend/src/modules/auth/pages/log-in-page.jsx
+++ apps/frontend/src/modules/auth/pages/log-in-page.jsx
@@ -1,5 +1,63 @@
-function LoginPage() {
-  return <h1>Login</h1>;
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+
+function LoginPage({ setToken, setUser }) {
+  const [username, setusername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch('http://localhost:3000/api/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await res.json();
+      if (res.ok) {
+        setToken(data.token);
+        setUser(data.user);
+        navigate('/login');
+      } else {
+        alert(data.message || 'Login failed');
+      }
+    } catch (error) {
+      console.error(error);
+      alert('Error connecting to server');
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <form onSubmit={handleLogin} className="bg-white p-6 rounded-xl shadow-md w-80">
+        <h2 className="text-2xl font-semibold text-center mb-6">Login</h2>
+
+        <input
+          type="text"
+          placeholder="username"
+          value={username}
+          onChange={(e) => setusername(e.target.value)}
+          required
+          className="w-full p-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          className="w-full p-2 mb-6 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
+        />
+
+        <button type="submit" className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
