54c018997ed3b02283f40a3d7df000a877711187
nerbertb <nerbertb@gmail.com>
2025-09-27 00:07:23 +0800

    fix: deleted profile-page. Creating new branch

--- apps/frontend/src/modules/home/pages/profile-page.jsx
+++
@@ -1,9 +0,0 @@
-function ProfilePage() {
-    return ( 
-        <div className='flex items-center justify-center'>
-        <div className='grid grid-cols-2 gap-4 bg-amber-200 h-[50vh] w-[80vw]'>Form</div>
-        </div>
-     );
-}
-
-export default ProfilePage;
