41bbaa072ed26b9850f5d37e23da6ba80f3e807e
Dean Harley Pal <paldeanharley@gmail.com>
2025-09-26 20:15:41 +0800

    feat: add validation for creating memory in the routes

--- apps/backend/src/modules/routes/memory-routes.js
+++ apps/backend/src/modules/routes/memory-routes.js
@@ -1,3 +1,5 @@
+import { authMiddleware } from '#modules/common/middleware/auth-create-Middleware.js';
+import { validateMemory } from '#modules/common/middleware/memory-middleware.js';
 import { createMemory } from '#modules/controllers/memory/create-memory.js';
 import { deleteMemory } from '#modules/controllers/memory/delete-memory.js';
 import { getMemories } from '#modules/controllers/memory/get-memories.js';
@@ -7,10 +9,10 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.post('/', authMiddleware, createMemory);
+router.post('/', authMiddleware, validateMemory, createMemory);
 router.get('/', authMiddleware, getMemories);
 router.get('/:id', authMiddleware, getMemoryById);
-router.put('/:id', authMiddleware, updateMemory);
+router.put('/:id', authMiddleware, validateMemory, updateMemory);
 router.delete('/:id', authMiddleware, deleteMemory);
 
 export default router;
