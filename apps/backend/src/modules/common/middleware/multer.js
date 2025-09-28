012ef926ca40b02f29e0782e7398130171503614
Dean Harley Pal <paldeanharley@gmail.com>
2025-09-28 15:34:54 +0800

    feat: import multer & add code explanation

---
+++ apps/backend/src/modules/common/middleware/multer.js
@@ -0,0 +1,22 @@
+// This is where picture files people upload
+// Think of it as the "middle" guy that takes a photo
+// and passes it to Cloudinary
+import { storage } from '#modules/config/cloudinary.js';
+import multer from 'multer';
+
+const upload = multer({
+  storage,
+  limits: {
+    // 5 MB
+    fileSize: 5 * 1024 * 1024
+  },
+  fileFilter: (req, file, callback) => {
+    const allowedTypes = ["image/jpeg", "image/png", "image/webp"];
+    if (!allowedTypes.includes(file.mimetype)) {
+      return callback(new Error("Only JPEG, PNG, and WEBP images are allowed"), false);
+    }
+    callback(null, true); // let the photo through if everything's okay
+  }
+})
+
+export default upload;
