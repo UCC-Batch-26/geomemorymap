5f7501bd038aabfba43e718297e88669f0ddd02a
nerbertb <nerbertb@gmail.com>
2025-10-01 02:35:25 +0800

    fix: commit to sync

--- apps/frontend/src/modules/home/pages/about-page.jsx
+++ apps/frontend/src/modules/home/pages/about-page.jsx
@@ -3,13 +3,14 @@ function AboutPage() {
     <section className="fixed bg-[url(@/assets/geo-memory-map-bg.png)] bg-size-[900px] bg-no-repeat bg-center h-screen w-full">
       <div className="bg-[#526b5c]/75 h-full">
         <div className="flex flex-col justify-center-safe gap-8 h-full text-left">
-          <h1 className="font-display md:text-4xl lg:text-7xl font-semibold text-white bg-gray-800/40 w-[35%] rounded-r-2xl pl-10">About</h1>
+          <h1 className="font-display md:text-4xl lg:text-7xl font-semibold text-white bg-gray-800/40 w-[35%] rounded-r-2xl pl-10">
+            About
+          </h1>
           <p className="font-display md:text-md lg:text-2xl text-gray-100 text-left max-w-3xl ml-10">
-            Geo Memory Map is designed to help users capture and preserve their most cherished memories in a unique and
-            meaningful way. By allowing users to pin moments directly onto an interactive map, the app connects stories,
-            photos, and descriptions to the specific places that hold significance in their lives. Whether it’s a
-            special event, a travel experience, or a personal milestone, Geo Memory Map transforms memories into visual
-            landmarks, making it easy to explore and share the stories that shape us. 
+            Geo Memory Map is all about helping you keep your favorite memories alive in a fun, meaningful way. With our
+            app, you can drop pins on a map to capture those special moments—whether it’s a trip, a celebration, or just
+            a place that means something to you. Add your stories, photos, and little details that bring your memories
+            to life right where they happened.
           </p>
         </div>
       </div>
