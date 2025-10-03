897c19efccc6d39c02e51a997f4094189c0c22f5
Dean Harley Pal <paldeanharley@gmail.com>
2025-10-04 01:17:21 +0800

    fix: possible fix for broken marker in prod

--- apps/frontend/src/modules/common/components/map-view.jsx
+++ apps/frontend/src/modules/common/components/map-view.jsx
@@ -2,6 +2,18 @@ import { useState, useEffect, useRef } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMapEvent, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import toast from 'react-hot-toast';
+import L from 'leaflet';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+})
 
 function RecenterMap({ center }) {
   const map = useMap();
