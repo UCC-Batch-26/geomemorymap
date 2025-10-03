4705ab21f16f0f977931ce1d32e27586f58350d5
Peter <pedrojuana08@gmail.com>
2025-10-03 17:46:58 +0800

    fix: pnpm fix

--- apps/frontend/src/modules/api-hooks/upload-image-cloudinary.js
+++ apps/frontend/src/modules/api-hooks/upload-image-cloudinary.js
@@ -14,7 +14,7 @@ export async function uploadImageToCloudinary(file) {
 
   if (!response.ok) {
     const errorText = await response.text();
-    console.error("Cloudinary upload failed:", errorText);
+    console.error('Cloudinary upload failed:', errorText);
     throw new Error('Failed to upload image');
   }
 
