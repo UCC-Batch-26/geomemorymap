99de66c60a883a377e24c74929c16391aea67908
Nathaniel Dacasin <nathandacasin01@gmail.com>
2025-10-05 22:45:21 +0800

    Updated design frontend Sign up

--- apps/frontend/src/modules/auth/pages/sign-up-page.jsx
+++ apps/frontend/src/modules/auth/pages/sign-up-page.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router';
 
 function SignUpPage() {
   const [form, setForm] = useState({
@@ -7,6 +9,8 @@ function SignUpPage() {
     password: '',
     age: '',
   });
+
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -21,7 +25,7 @@ function SignUpPage() {
     const age = form.age;
 
     if (!username || !email || !password) {
-      alert('Please fill in all required fields.');
+      toast.error('Please fill in all required fields.');
       return;
     }
 
@@ -33,67 +37,87 @@ function SignUpPage() {
       });
 
       const data = await res.json();
-      console.log(data);
 
       if (res.ok) {
-        alert('Registered successfully! You can login now.');
+        toast.success('Registered successfully! Redirecting to login...');
+        setTimeout(() => navigate('/login'), 1500);
         setForm({ username: '', email: '', password: '', age: '' });
       } else {
-        alert(data.message || 'Registration failed');
+        toast.error(data.message || 'Registration failed');
       }
     } catch (error) {
       console.error('Registration error:', error);
-      alert('Error connecting to server');
+      toast.error('Error connecting to server');
     }
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <form className="bg-white border border-black rounded-lg p-6 w-80 shadow-md" onSubmit={handleRegister}>
-        <h2 className="text-xl font-bold mb-4 text-center">Register</h2>
+    <div className="fixed bg-[url(@/assets/geo-memory-map-bg.png)] bg-size-[600px] bg-no-repeat bg-center">
+      <div className="grid grid-cols-4 bg-[#526b5c]/80 h-[100vh]">
+        {/* LEFT SECTION */}
+        <div className="col-start-2 row-start-2">
+          <h1 className="font-[Montserrat] text-6xl font-bold text-white text-left">Sign Up</h1>
+          <h3 className="font-[Montserrat] text-2xl font-bold text-white text-left py-5">Create your account</h3>
+          <h4 className="font-[Montserrat] text-md text-white text-left py-20">
+            Join the map of memories — explore, connect, and share your own world’s stories!
+          </h4>
+        </div>
 
-        <input
-          name="username"
-          placeholder="Username"
-          onChange={handleChange}
-          required
-          className="w-full px-3 py-2 mb-3 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
-        />
+        {/* RIGHT SECTION (FORM) */}
+        <div className="place-items-center col-start-3 row-start-2">
+          <form onSubmit={handleRegister} className="bg-white/50 p-6 rounded-xl shadow-md w-80 h-100">
+            <h2 className="text-3xl font-bold text-gray-100 text-shadow-2xs text-center mb-6 font-[Montserrat]">
+              Sign Up
+            </h2>
 
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          onChange={handleChange}
-          required
-          className="w-full px-3 py-2 mb-3 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
-        />
+            <input
+              name="username"
+              placeholder="Username"
+              value={form.username}
+              onChange={handleChange}
+              required
+              className="w-full p-2 mb-4 border border-gray-100 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
+            />
 
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          onChange={handleChange}
-          required
-          className="w-full px-3 py-2 mb-3 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
-        />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={form.email}
+              onChange={handleChange}
+              required
+              className="w-full p-2 mb-4 border border-gray-100 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
+            />
 
-        <input
-          type="number"
-          name="age"
-          placeholder="Age"
-          onChange={handleChange}
-          required
-          className="w-full px-3 py-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
-        />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={form.password}
+              onChange={handleChange}
+              required
+              className="w-full p-2 mb-4 border border-gray-100 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
+            />
 
-        <button
-          type="submit"
-          className="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 transition"
-        >
-          Register
-        </button>
-      </form>
+            <input
+              type="number"
+              name="age"
+              placeholder="Age"
+              value={form.age}
+              onChange={handleChange}
+              required
+              className="w-full p-2 mb-6 border border-gray-100 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
+            />
+
+            <button
+              type="submit"
+              className="w-full bg-[#EF6B48] text-white py-2 mb-8 rounded-md hover:bg-[#e9542b] transition"
+            >
+              Register
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
